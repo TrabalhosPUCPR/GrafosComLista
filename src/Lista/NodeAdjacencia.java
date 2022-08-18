7eaebc22e3b2ebd2bf190226ea3b5fd1de3ff8aa
KnightLeo <knightleo602@gmail.com>
2022-08-18 09:21:48 -0300

    Update NodeAdjacencia.java

--- src/Lista/NodeAdjacencia.java
+++ src/Lista/NodeAdjacencia.java
@@ -2,7 +2,7 @@ package Lista;
 
 public class NodeAdjacencia extends Node{
     int peso;
-    public NodeAdjacencia(String valor, Node next, Node last, int peso) {
+    public NodeAdjacencia(String valor, Node<?> next, Node<?> last, int peso) {
         super(valor, next, last);
         this.peso = peso;
     }
